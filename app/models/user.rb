502395433bb34d2438bc4940c7dec9c3c976a92e
DiptoSarkar182 <diptomanchester2018@gmail.com>
2024-04-18 22:34:57 +0600

    added user vaidation

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,7 @@ class User < ApplicationRecord
     "#{first_name} #{last_name} - #{speciality}"
   end
   validates :role, presence: true, inclusion: { in: ['doctor', 'receptionist'] }
+  validates :first_name, :last_name, presence: true
   has_many :registered_patients, class_name: 'Patient', foreign_key: 'registered_by_id'
   has_many :patients, foreign_key: 'doctor_id'
 end
